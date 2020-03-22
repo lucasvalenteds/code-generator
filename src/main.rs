6b5abb33ff9ebe5bbb068c1688d9df9508c2da2d
Lucas <lucasvalenteds@gmail.com>
2020-03-22 20:36:36 -0300

    Implements simple CLI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 mod generator;
 
 use generator::*;
+use std::env;
 
 fn main() {
-    for _ in 0..20 {
+    let args: Vec<String> = env::args().collect::<Vec<String>>();
+
+    if args.len() == 2 {
+        let argument: String = args[1].to_owned();
+
+        if argument.len() == 2 {
+            match generate_code(Some(&argument), None) {
+                Some(code) => println!("{}", code),
+                None => panic!("Could not generate code"),
+            }
+        } else {
+            match generate_code(None, Some(&argument)) {
+                Some(code) => println!("{}", code),
+                None => panic!("Could not generate code"),
+            }
+        }
+    } else {
         match generate_code(None, None) {
             Some(code) => println!("{}", code),
             None => panic!("Could not generate code"),
