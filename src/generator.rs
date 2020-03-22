1bde64b446aa35eeba829cadba5a9b8dd1ba7a5d
Lucas <lucasvalenteds@gmail.com>
2020-03-22 19:17:37 -0300

    Rewrite algorithm using a functional approach

--- src/generator.rs
+++ src/generator.rs
@@ -21,37 +21,30 @@ pub fn generate_code(seed: Option<&str>) -> Option<String> {
         ),
     };
 
-    if code.len() != 16 {
+    let digits: Vec<u32> = code
+        .chars()
+        .filter_map(|character: char| character.to_digit(16))
+        .collect();
+
+    if digits.len() != 16 {
         return None;
     }
 
-    let characters: Vec<char> = code.chars().collect();
+    let first_digit: u32 = digits
+        .clone()
+        .into_iter()
+        .enumerate()
+        .map(|(index, digit)| ((index as u32) + 1) * digit)
+        .sum();
 
-    let mut characters_as_hex: Vec<u32> = Vec::with_capacity(16);
-    for index in 0..16 {
-        if let Some(character_as_hex) = characters[index].to_digit(16) {
-            characters_as_hex.push(character_as_hex);
-        } else {
-            return None;
-        }
-    }
+    let second_digit: u32 = digits
+        .clone()
+        .into_iter()
+        .enumerate()
+        .map(|(index, digit)| (index as u32) * digit)
+        .sum();
 
-    let mut first_digit_sequence: Vec<u32> = Vec::with_capacity(16);
-    for index in 0..characters_as_hex.len() {
-        first_digit_sequence.push(characters_as_hex[index] * (index + 1) as u32);
-    }
-    let first_digit: u32 = (first_digit_sequence.iter().sum::<u32>()) % 16;
-
-    let mut second_digit_sequence: Vec<u32> = Vec::with_capacity(16);
-    for index in 0..characters_as_hex.len() {
-        second_digit_sequence.push(characters_as_hex[index] * index as u32);
-    }
-    let second_digit: u32 = (second_digit_sequence.iter().sum::<u32>()) % 16;
-
-    characters_as_hex.push(first_digit);
-    characters_as_hex.push(second_digit);
-
-    Some(format!("{}{}{}", code, first_digit, second_digit))
+    Some(format!("{}{}{}", code, first_digit % 16, second_digit % 16))
 }
 
 #[cfg(test)]
