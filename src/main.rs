5c0df19c022b91c498b27a323cbbda7dfc464382
Lucas <lucasvalenteds@gmail.com>
2020-03-21 23:40:31 -0300

    Rewrite main function to print code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
 mod generator;
 
 fn main() {
-    let seed = Some("00124b00188a7f68");
-    if let Some(code) = generator::generate_code(seed) {
-        println!("{}", code)
-    } else {
-        println!("Could not generate code")
-    }
-
-    if let Some(code) = generator::generate_code(None) {
-        println!("{}", code)
-    } else {
-        println!("Could not generate code")
+    match generator::generate_code(None) {
+        Some(code) => println!("{}", code),
+        None => panic!("Could not generate code"),
     }
 }
